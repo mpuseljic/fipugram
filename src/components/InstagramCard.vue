c3625af4544decfc58167487d618e6b179375f21
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-04-05 18:54:47 +0200

    Create InstagramCard.vue

---
+++ src/components/InstagramCard.vue
@@ -0,0 +1,22 @@
+<template>
+<div class="card text-center">
+  <div class="card-header">
+    Featured
+  </div>
+  <div class="card-body p-0">
+    <img class = "card-img-top" src="https://picsum.photos/800"/>
+  </div>
+  <div class="card-footer text-body-secondary">
+    2 days ago
+  </div>
+</div>
+</template>
+
+<script>
+// JS kod
+export default {
+  name: 'InstagramCard', 
+}
+</script>
+
+
